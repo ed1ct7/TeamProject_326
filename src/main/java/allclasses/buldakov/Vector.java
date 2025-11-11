703ccb64107100878916dd589fa4fb046cb717d0
barsik-47 <alekzzandr07@gmail.com>
2025-11-11 14:26:51 +0300

    added class vector, vector test class

---
+++ src/main/java/allclasses/buldakov/Vector.java
@@ -0,0 +1,29 @@
+package allclasses.buldakov;
+
+public class Vector {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getLength() {
+        return Math.sqrt(x*x + y*y);
+    }
+}
