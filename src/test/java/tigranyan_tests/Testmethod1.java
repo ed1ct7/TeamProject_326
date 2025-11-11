d6f2a422473d741f382d47351322c43e85f2cd26
ed1ct7 <edik.tigranyan@yandex.ru>
2025-11-11 13:45:41 +0300

    example test method

---
+++ src/test/java/tigranyan_tests/Testmethod1.java
@@ -0,0 +1,11 @@
+package tigranyan_tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testmethod1 {
+@Test
+    public void TestmethodOne(){
+    Assert.assertTrue(true);
+}
+}
