eed99c552f34bad4f55f098b6da1b2b78cb30d0e
c0dev1 <pkikirillpki666@gmail.com>
2025-11-18 13:54:19 +0300

    added class ships

--- src/main/java/allclasses/poruchikov/ships.java
+++ src/main/java/allclasses/poruchikov/ships.java
@@ -17,6 +17,31 @@ public class ships {
         this.name = name;
         this.type = type;
         this.year = year;
-        this.weight = weight;
+        this.weight = weight;A
     }
+
+    public int calculateAge() {
+        return 2024 - year;
+    }
+
+    public boolean isOldShip() {
+        return calculateAge() > 20;
+    }
+
+    public void printShipInfo() {
+        System.out.println("Корабль: " + name);
+        System.out.println("Тип: " + type);
+        System.out.println("Год постройки: " + year);
+        System.out.println("Вес: " + weight + " тонн");
+    }
+
+    public double calculateFuelEfficiency() {
+        double baseEfficiency = 100; // км/т топлива
+        double agePenalty = calculateAge() * 0.5;
+        double weightPenalty = weight * 0.001;
+        double typeBonus = getTypeEfficiencyBonus();
+
+        return Math.max(10, baseEfficiency - agePenalty - weightPenalty + typeBonus);
+    }
+
 }
