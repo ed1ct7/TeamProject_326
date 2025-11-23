5ff69e80daf4c1744b8e7baec0df29608c489ad5
nastarozina <nastarozina221@gmail.com>
2025-11-23 18:09:43 +0300

    Wrote main static func

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import allclasses.bartasevich.Toy;
+import allclasses.rozhina.Phone;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,6 +10,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         tigranyan();
         bartasevichMethod();
+        rozhinaMethod();
 
     }
 
@@ -26,5 +28,25 @@ public class Main {
         System.out.println(toy.ToString());
         System.out.println(toy1.ToString());
 
+    }
+    public static void rozhinaMethod() {
+        Phone phone1 = new Phone();
+        Phone phone2 = new Phone("iPhone", "16 Pro Max", 104490, 256, 10);
+        System.out.println("\nТелефон 1: " + phone1.ToString());
+        System.out.println("Телефон 2: " + phone2.ToString());
+        System.out.println("Дешевый ли первый телефон - " + phone1.isCheapPrice());
+        System.out.println("Дешевый ли второй телефон - " + phone2.isCheapPrice());
+        System.out.println("Мало ли памяти у первого телефона - " + phone1.isLittleMemory());
+        System.out.println("Мало ли памяти у второго телефона - " + phone2.isLittleMemory());
+        System.out.println("Низкий ли заряд у первого телефона - " + phone1.isLowBattery());
+        System.out.println("Низкий ли заряд у второго телефона - " + phone2.isLowBattery());
+        phone1.chargeToFull();
+        System.out.println("Первый телефон полностью заряжен, заряд: " + phone1.getBattery());
+        phone2.chargeToFull();
+        System.out.println("Второй телефон полностью заряжен, заряд: " + phone2.getBattery());
+        phone1.dischargeFully();
+        System.out.println("Первый телефон полностью разряжен, заряд: " + phone1.getBattery());
+        phone2.dischargeFully();
+        System.out.println("Второй телефон полностью разряжен, заряд: " + phone2.getBattery());
     }
 }
