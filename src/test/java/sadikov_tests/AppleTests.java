86775b9b9a8f41b5fadb3b597c67625223c6e494
EgorSad06 <143409348+EgorSad06@users.noreply.github.com>
2025-11-24 17:23:17 +0300

    added apple class, tests for apples and static method

--- src/test/java/sadikov_tests/AppleTests.java
+++ src/test/java/sadikov_tests/AppleTests.java
@@ -1,102 +1,62 @@
 package sadikov_tests;
 
 import allclasses.sadikov.Apple;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
 
 public class AppleTests {
 
-    public static void main(String[] args) {
-        testCanEat();
-        testTakeBite();
-        testSweetnessAndWeightComparison();
-        testSettersValidation();
-        System.out.println("Все тесты завершены");
+    private Apple redApple;
+    private Apple greenApple;
+
+    @Before
+    public void setUp() {
+        redApple = new Apple("red", 200.0, true, "Gala", 8.2);
+        greenApple = new Apple("green", 150.0, false, "Granny Smith", 4.5);
     }
 
-    public static void testCanEat() {
-        Apple redApple = new Apple("red", 200.0, true, "Gala", 8.2);
-        Apple greenApple = new Apple("green", 150.0, false, "Granny Smith", 4.5);
+    @Test
+    public void testCanEat() {
+        assertTrue(redApple.canEat());
+        assertFalse(greenApple.canEat());
+
         Apple zeroWeightApple = new Apple("yellow", 0.0, true, "Golden", 6.0);
-
-        boolean test1 = redApple.canEat();
-        boolean test2 = greenApple.canEat();
-        boolean test3 = zeroWeightApple.canEat();
-
-        if (test1 && !test2 && !test3) {
-            System.out.println("Тест 1 пройден");
-        } else {
-            System.out.println("Тест 1 провален");
-        }
+        assertFalse(zeroWeightApple.canEat());
     }
 
-    public static void testTakeBite() {
-        Apple apple = new Apple("red", 200.0, true, "Gala", 8.2);
-        double initialWeight = apple.getWeight();
+    @Test
+    public void testTakeBite() {
+        double initialWeight = redApple.getWeight();
+        redApple.takeBite(50.0);
 
-        apple.takeBite(50.0);
-        double weightAfterBite = apple.getWeight();
+        assertEquals(initialWeight - 50.0, redApple.getWeight(), 0.001);
 
-        apple.takeBite(300.0);
-        double finalWeight = apple.getWeight();
-
-        boolean testPassed = (weightAfterBite == initialWeight - 50.0) && (finalWeight == 0.0);
-
-        if (testPassed) {
-            System.out.println("Тест 2 пройден");
-        } else {
-            System.out.println("Тест 2 провален");
-        }
+        redApple.takeBite(300.0);
+        assertEquals(0.0, redApple.getWeight(), 0.001);
     }
 
-    public static void testSweetnessAndWeightComparison() {
-        Apple redApple = new Apple("red", 200.0, true, "Gala", 8.2);
-        Apple greenApple = new Apple("green", 150.0, false, "Granny Smith", 4.5);
-        Apple sweetApple = new Apple("red", 100.0, true, "Sweet", 6.0);
-        Apple sourApple = new Apple("green", 100.0, true, "Sour", 3.9);
+    @Test
+    public void testSweetnessAndWeightComparison() {
+        assertEquals("очень сладкое", redApple.getSweetnessCategory());
+        assertEquals("кисло-сладкое", greenApple.getSweetnessCategory());
 
-        String category1 = redApple.getSweetnessCategory();
-        String category2 = greenApple.getSweetnessCategory();
-        String category3 = sweetApple.getSweetnessCategory();
-        String category4 = sourApple.getSweetnessCategory();
-
-        boolean isHeavier = redApple.isHeavierThan(greenApple);
-        boolean isNotHeavier = greenApple.isHeavierThan(redApple);
-
-        boolean sweetnessTest = category1.equals("очень сладкое") &&
-                category2.equals("кисло-сладкое") &&
-                category3.equals("сладкое") &&
-                category4.equals("кислое");
-
-        boolean weightTest = isHeavier && !isNotHeavier;
-
-        if (sweetnessTest && weightTest) {
-            System.out.println("Тест 3 пройден");
-        } else {
-            System.out.println("Тест 3 провален");
-        }
+        assertTrue(redApple.isHeavierThan(greenApple));
+        assertFalse(greenApple.isHeavierThan(redApple));
     }
 
-    public static void testSettersValidation() {
-        Apple apple = new Apple();
-        double initialWeight = apple.getWeight();
-        double initialSweetness = apple.getSweetness();
+    @Test
+    public void testSettersValidation() {
+        Apple testApple = new Apple();
+        double initialSweetness = testApple.getSweetness();
 
-        apple.setWeight(-10.0);
-        double weightAfterInvalid = apple.getWeight();
+        testApple.setWeight(-10.0);
+        assertTrue(testApple.getWeight() >= 0);
 
-        apple.setSweetness(15.0);
-        double sweetnessAfterInvalid = apple.getSweetness();
+        testApple.setSweetness(15.0);
+        assertEquals(initialSweetness, testApple.getSweetness(), 0.001);
 
-        apple.setWeight(100.0);
-        apple.setSweetness(5.0);
-
-        boolean weightValid = weightAfterInvalid == initialWeight;
-        boolean sweetnessValid = sweetnessAfterInvalid == initialSweetness;
-        boolean finalValuesCorrect = apple.getWeight() == 100.0 && apple.getSweetness() == 5.0;
-
-        if (weightValid && sweetnessValid && finalValuesCorrect) {
-            System.out.println("Тест 4 пройден");
-        } else {
-            System.out.println("Тест 4 провален");
-        }
+        testApple.setSweetness(5.0);
+        assertEquals(5.0, testApple.getSweetness(), 0.001);
     }
 }
