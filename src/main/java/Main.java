28912d9c972c4958222d72e1c6fb23a36d9d22af
Varsipass <ivanez2007@gmail.com>
2025-12-02 13:38:28 +0300

    school repository class fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -170,8 +170,8 @@ public class Main {
         component1.PrintSpecsInfo();
         System.out.print(component1.CalculateFreeMemory(2047));
         System.out.print("\n");
-        System.out.print(component1.GetRublePrice());
-        System.out.print("=================================");
+        System.out.println(component1.GetRublePrice());
+        System.out.println("=================================");
     }
 
     public  static  void ezerskiyMethod(){
