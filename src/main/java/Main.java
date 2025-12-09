cea7a32465ddb4dba5be471792a9a9b7ccf329bd
viruy04 <annakazakow4@yandex.ru>
2025-12-09 10:36:10 +0300

    add display

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     public static void kazakova(){
         Coffee coffee = new Coffee("Раф", 1);
-        System.out.println("\nКазакова, "+ coffee.getCoffee());
+        System.out.println("\nКазакова, "+ coffee.getCoffee());}
     public static void buldakovMethod() {
         Vector vec1 = new Vector();
         Vector vec2 = new Vector(1.5,2);
