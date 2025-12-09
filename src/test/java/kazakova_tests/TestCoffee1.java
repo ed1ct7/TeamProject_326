e5e6a1b9baa0856fc7e95dfc1f7723a1f23d753a
viruy04 <annakazakow4@yandex.ru>
2025-12-09 10:36:57 +0300

    add display 2

--- src/test/java/kazakova_tests/TestCoffee1.java
+++ src/test/java/kazakova_tests/TestCoffee1.java
@@ -2,16 +2,19 @@ package kazakova_tests;
 
 import allclasses.kazakova.Coffee;
 import org.junit.Test;
+import io.qameta.allure.junit4.DisplayName;
 import static org.junit.Assert.*;
 
 public class TestCoffee1 {
     @Test
+    @DisplayName("Проверка создания кофе")
     public void buyCoffee() {
         Coffee coffee1 = new Coffee("Latte", 5);
         assertEquals("Кофе: Latte, сахара: 5 ложек.", coffee1.getCoffee());
     }
 
     @Test
+    @DisplayName("Проверка добавления сахара")
     public void addSugarTest() {
         Coffee coffee = new Coffee("Americano", 2);
         String result = coffee.addSugar(3);
